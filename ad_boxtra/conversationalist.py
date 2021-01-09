e7769760d7aa12c30ad715253620ec9f12168bd8
Ryan Lewis <ryandlewis.rl@gmail.com>
2021-01-09 22:32:37 +0000

    Conversationalist can now subscribe to topics

--- ad_boxtra/conversationalist.py
+++ ad_boxtra/conversationalist.py
@@ -1,7 +1,25 @@
 import datetime
 import socket
+import rclpy
+from rclpy.node import Node
+from std_msgs.msg import String
 
-def main():
+class Subscriber(Node):
+    def __init__(self):
+        super().__init__('conversationalist')
+        self.subscription = self.create_subscription(
+                String,
+                'logger',
+                self.listener_callback,
+                10)
+        self.subscription # prevent unused variable warning
+
+    def listener_callback(self, msg):
+        print('I heard: "%s"' % msg.data)
+
+def main(args=None):
+    rclpy.init(args=args)
+    sub = Subscriber()
     print("+----------------------------------------------------------------------------------+")
     print("|             👋 Hello! My name is Ad Boxtra, the boxy rover from NIU.             |")
     print("|                                                                                  |")
@@ -14,6 +32,9 @@ def main():
     print("|                                                                                  |")
     print("| You can help build me at https://niurover.com or https://github.com/NIURoverTeam |")
     print("+----------------------------------------------------------------------------------+")
+    print("Now I'm listening to the '/logger' topic. You can publish messages there with:")
+    print("ros2 topic pub --once /logger std_msgs/msg/String \"{data: '<your message>'}\"")
+    rclpy.spin(sub)
 
 if __name__ == '__main__':
     main()
