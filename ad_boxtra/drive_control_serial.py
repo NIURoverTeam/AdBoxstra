0c62fb0da63db6099cb62bc4b918030efcdfc532
Ryan Lewis <ryandlewis.rl@gmail.com>
2021-03-20 15:04:55 -0500

    Documentation and dependencies

--- ad_boxtra/drive_control_serial.py
+++ ad_boxtra/drive_control_serial.py
@@ -7,6 +7,7 @@ from std_msgs.msg import String
 from sensor_msgs.msg import Joy
 from serial import Serial
 
+# Create a serial object: Serial(file handle for an arduino, baud rate)
 ser = Serial("/dev/ttyACM0", 9600)
 
 class Subscriber(Node):
@@ -20,11 +21,12 @@ class Subscriber(Node):
         self.subscription # prevent unused variable warning
 
     def on_joy_msg(self, data):
-        leftPower = round(data.axes[1] * 250 / 2)
-        rightPower = round(data.axes[4] * 250 / 2)
-        leftDir = 0x4C if leftPower >= 0 else 0x6C
-        rightDir = 0x52 if rightPower >= 0 else 0x72
+        leftPower = round(data.axes[1] * 250 / 2) # int from 0 to 125 for left talon
+        rightPower = round(data.axes[4] * 250 / 2) # int from 0 to 125 for right talon
+        leftDir = 0x4C if leftPower >= 0 else 0x6C # 'L' (forward) or 'l' (backward)
+        rightDir = 0x52 if rightPower >= 0 else 0x72 # 'R' or 'r'
 
+        # Writes our 4-byte packet over serial
         ser.write(
                 struct.pack(
                     ">BBBB", leftDir, int(abs(leftPower)), rightDir, int(abs(rightPower))
@@ -33,6 +35,7 @@ class Subscriber(Node):
 
 def main(args=None):
     rclpy.init(args=args)
+    # Create our /joy listener
     sub = Subscriber()
     rclpy.spin(sub)
 
